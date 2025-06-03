4fe8e637bd9716a733e6e804a70aa3ddc42522b2
Wei Lu <way138730101@gmail.com>
2025-06-03 14:19:43 -0400

    added boost and bumps

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float steerSpeed = 100f;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float slowSpeed = 7f;
+    [SerializeField] float boostSpeed = 20f;
 
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
-        transform.Translate(0, moveAmount, 0); 
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
     }
 }
